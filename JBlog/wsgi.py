867fecc2c2a521fa98da52a6b4648067463929e0
CJP <498882147@qq.com>
2019-04-12 10:39:55 +0800

    test
    加一减一

--- JBlog/wsgi.py
+++ JBlog/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+# test
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "JBlog.settings")
